ee6a4795ca2b54e19da978336c16e3758d9b16c9
roman <roman-1901@mail.ru>
2023-04-23 16:14:55 +0300

    Initial commit

---
+++ src/main/java/com/example/usertest2/UserTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.usertest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usertest2/UserTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usertest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
